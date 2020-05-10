27f5c935c983ebb24928befff845cd254c214bd8
ellaHRM <modellastar@gmail.com>
2020-05-11 02:29:01 +0300

    слайдер фабрика

--- slider/dist/slider.dev.js
+++ slider/dist/slider.dev.js
@@ -1,39 +1,49 @@
 "use strict";
 
-var slider = {
-  currentImageIndex: 0,
-  clickPrevBtn: document.getElementById('prevClick'),
-  clickNextBtn: document.getElementById('nextClick'),
-  imgSlide: document.getElementById('img-slide'),
-  imagesUrls: ['https://static.depositphotos.com/storage/image/419793f67b6178413165f6419e9d60786ef96c95.jpg', 'https://static.depositphotos.com/storage/image/44e659e46ed7aa3758ca03435def4af874bcfced.jpg', 'https://static.depositphotos.com/storage/image/61a9b826aa1b96fbc0a23439d50b826dbcd74339.jpg', 'https://static.depositphotos.com/storage/image/c90c6bbe5c48db0879ec8c5397b77f254736fc47.jpg'],
-  init: function init() {
-    var the = this; // subscribe to events
+var sliderFactory = {
+  getSlider: function getSlider() {
+    var slider = {
+      currentImageIndex: 0,
+      clickPrevBtn: null,
+      clickNextBtn: null,
+      imgSlide: null,
+      imagesUrls: null,
+      init: function init(elId, images) {
+        var the = this;
+        var el = document.querySelector('#' + elId);
+        this.clickPrevBtn = el.querySelector('.prevClick');
+        this.clickNextBtn = el.querySelector('.nextClick');
+        this.imgSlide = el.querySelector('.img-slide');
+        this.imagesUrls = images; // subscribe to events
 
-    this.clickPrevBtn.addEventListener('click', function (e) {
-      the.onClickPrev(e);
-    });
-    this.clickNextBtn.addEventListener('click', function (e) {
-      the.onClickNext(e);
-    });
-    this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
-    this.clickPrevBtn.disabled = true;
-  },
-  onClickPrev: function onClickPrev(e) {
-    this.clickNextBtn.disabled = false;
-    this.currentImageIndex--;
-    this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+        this.clickPrevBtn.addEventListener('click', function (e) {
+          the.onClickPrev(e);
+        });
+        this.clickNextBtn.addEventListener('click', function (e) {
+          the.onClickNext(e);
+        });
+        this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+        this.clickPrevBtn.disabled = true;
+      },
+      onClickPrev: function onClickPrev(e) {
+        this.clickNextBtn.disabled = false;
+        this.currentImageIndex--;
+        this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
 
-    if (this.currentImageIndex === 0) {
-      this.clickPrevBtn.disabled = true;
-    }
-  },
-  onClickNext: function onClickNext(e) {
-    this.clickPrevBtn.disabled = false;
-    this.currentImageIndex++;
-    this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+        if (this.currentImageIndex === 0) {
+          this.clickPrevBtn.disabled = true;
+        }
+      },
+      onClickNext: function onClickNext(e) {
+        this.clickPrevBtn.disabled = false;
+        this.currentImageIndex++;
+        this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
 
-    if (this.currentImageIndex === this.imagesUrls.length - 1) {
-      this.clickNextBtn.disabled = true;
-    }
+        if (this.currentImageIndex === this.imagesUrls.length - 1) {
+          this.clickNextBtn.disabled = true;
+        }
+      }
+    };
+    return slider;
   }
 };
--- slider/js.js
+++ slider/js.js
@@ -1,2 +1,10 @@
-slider.init();
+var images1 = ['https://static.depositphotos.com/storage/image/419793f67b6178413165f6419e9d60786ef96c95.jpg', 'https://static.depositphotos.com/storage/image/44e659e46ed7aa3758ca03435def4af874bcfced.jpg', 'https://static.depositphotos.com/storage/image/61a9b826aa1b96fbc0a23439d50b826dbcd74339.jpg', 'https://static.depositphotos.com/storage/image/c90c6bbe5c48db0879ec8c5397b77f254736fc47.jpg'];
+var images2 = ['https://static.depositphotos.com/storage/image/5fa95797d0438bbecf4c0f581c5ec5676a304c9e.jpg', 'https://static.depositphotos.com/storage/image/5946f7157f087c5ee01f0b59fe9ca057579ee69f.jpg', 'https://static.depositphotos.com/storage/image/61a9b826aa1b96fbc0a23439d50b826dbcd74339.jpg', 'https://static.depositphotos.com/storage/image/c90c6bbe5c48db0879ec8c5397b77f254736fc47.jpg'];
+var slider1 = sliderFactory.getSlider();
+var slider2 = sliderFactory.getSlider();
+
+slider1.init('slider1',images1);
+slider2.init('slider2',images2);
 //console.log(Object.keys(slider));
+// var myCrazyObject = {"name": "Нелепыйобъект","some array": [7, 9, { purpose: "путаница", number: 123 }, 3.3],"random animal": "Банановаяакула"};
+// console.log(myCrazyObject["some array"][2].number);
