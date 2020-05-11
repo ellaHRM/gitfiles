083d2b684139f9c467022d597fc5b3b527ecd1d9
ellaHRM <modellastar@gmail.com>
2020-05-12 00:48:41 +0300

    слайдер как объект new

--- slider/js.js
+++ slider/js.js
@@ -5,6 +5,7 @@ var slider2 = new Slider();
 
 slider1.init('slider1',images1);
 slider2.init('slider2',images2);
+
 //console.log(Object.keys(slider));
 // var myCrazyObject = {"name": "Нелепыйобъект","some array": [7, 9, { purpose: "путаница", number: 123 }, 3.3],"random animal": "Банановаяакула"};
 // console.log(myCrazyObject["some array"][2].number);
