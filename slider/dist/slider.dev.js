72182404a0f8165ed42921e814a0fdee0ee01388
ellaHRM <modellastar@gmail.com>
2020-05-12 00:46:11 +0300

    Revert "тест"
    This reverts commit 05ad02e28adafb37c91411e716452ef39fcb282a.

--- slider/dist/slider.dev.js
+++ slider/dist/slider.dev.js
@@ -1,49 +1,93 @@
 "use strict";
 
-var sliderFactory = {
-  getSlider: function getSlider() {
-    var slider = {
-      currentImageIndex: 0,
-      clickPrevBtn: null,
-      clickNextBtn: null,
-      imgSlide: null,
-      imagesUrls: null,
-      init: function init(elId, images) {
-        var the = this;
-        var el = document.querySelector('#' + elId);
-        this.clickPrevBtn = el.querySelector('.prevClick');
-        this.clickNextBtn = el.querySelector('.nextClick');
-        this.imgSlide = el.querySelector('.img-slide');
-        this.imagesUrls = images; // subscribe to events
+function Slider() {
+  this.currentImageIndex = 0;
+  this.clickPrevBtn = null;
+  this.clickNextBtn = null;
+  this.imgSlide = null;
+  this.imagesUrls = null;
 
-        this.clickPrevBtn.addEventListener('click', function (e) {
-          the.onClickPrev(e);
-        });
-        this.clickNextBtn.addEventListener('click', function (e) {
-          the.onClickNext(e);
-        });
-        this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
-        this.clickPrevBtn.disabled = true;
-      },
-      onClickPrev: function onClickPrev(e) {
-        this.clickNextBtn.disabled = false;
-        this.currentImageIndex--;
-        this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+  this.init = function (elId, images) {
+    var the = this;
+    var el = document.querySelector('#' + elId);
+    this.clickPrevBtn = el.querySelector('.prevClick');
+    this.clickNextBtn = el.querySelector('.nextClick');
+    this.imgSlide = el.querySelector('.img-slide');
+    this.imagesUrls = images; // subscribe to events
 
-        if (this.currentImageIndex === 0) {
-          this.clickPrevBtn.disabled = true;
-        }
-      },
-      onClickNext: function onClickNext(e) {
-        this.clickPrevBtn.disabled = false;
-        this.currentImageIndex++;
-        this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+    this.clickPrevBtn.addEventListener('click', function (e) {
+      the.onClickPrev(e);
+    });
+    this.clickNextBtn.addEventListener('click', function (e) {
+      the.onClickNext(e);
+    });
+    this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+    this.clickPrevBtn.disabled = true;
+  };
 
-        if (this.currentImageIndex === this.imagesUrls.length - 1) {
-          this.clickNextBtn.disabled = true;
-        }
-      }
-    };
-    return slider;
-  }
-};
+  this.onClickPrev = function (e) {
+    this.clickNextBtn.disabled = false;
+    this.currentImageIndex--;
+    this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+
+    if (this.currentImageIndex === 0) {
+      this.clickPrevBtn.disabled = true;
+    }
+  };
+
+  this.onClickNext = function (e) {
+    this.clickPrevBtn.disabled = false;
+    this.currentImageIndex++;
+    this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+
+    if (this.currentImageIndex === this.imagesUrls.length - 1) {
+      this.clickNextBtn.disabled = true;
+    }
+  };
+}
+
+; // var sliderFactory = {
+//     getSlider: function () {
+//         var slider = {
+//             currentImageIndex: 0,
+//             clickPrevBtn: null,
+//             clickNextBtn: null,
+//             imgSlide: null,
+//             imagesUrls: null,
+//             init: function (elId, images) {
+//                 var the = this;
+//                 var el = document.querySelector('#' + elId);
+//                 this.clickPrevBtn = el.querySelector('.prevClick');
+//                 this.clickNextBtn = el.querySelector('.nextClick');
+//                 this.imgSlide = el.querySelector('.img-slide');
+//                 this.imagesUrls = images;
+//                 // subscribe to events
+//                 this.clickPrevBtn.addEventListener('click', function (e) {
+//                     the.onClickPrev(e)
+//                 });
+//                 this.clickNextBtn.addEventListener('click', function (e) {
+//                     the.onClickNext(e);
+//                 });
+//                 this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+//                 this.clickPrevBtn.disabled = true;
+//             },
+//             onClickPrev: function (e) {
+//                 this.clickNextBtn.disabled = false;
+//                 this.currentImageIndex--;
+//                 this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+//                 if (this.currentImageIndex === 0) {
+//                     this.clickPrevBtn.disabled = true;
+//                 }
+//             },
+//             onClickNext: function (e) {
+//                 this.clickPrevBtn.disabled = false;
+//                 this.currentImageIndex++;
+//                 this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+//                 if (this.currentImageIndex === (this.imagesUrls.length - 1)) {
+//                     this.clickNextBtn.disabled = true;
+//                 }
+//             }
+//         };
+//         return slider;
+//     }
+// };
--- slider/slider.js
+++ slider/slider.js
@@ -1,56 +1,105 @@
-var sliderFactory = {
-    getSlider: function(){
-        var slider = {
-            currentImageIndex: 0,
-            clickPrevBtn: null,
-            clickNextBtn: null,
-            imgSlide: null,
-            imagesUrls: null,
-            
-            init: function (elId,images) {
-                var the = this;
-                var el = document.querySelector('#'+elId);
-        
-                this.clickPrevBtn = el.querySelector('.prevClick');
-                this.clickNextBtn = el.querySelector('.nextClick');
-                this.imgSlide = el.querySelector('.img-slide');
-                this.imagesUrls = images;
-        
-        
-                // subscribe to events
-                this.clickPrevBtn.addEventListener('click', function(e){
-                    the.onClickPrev(e)
-                });
-                this.clickNextBtn.addEventListener('click', function(e){
-                    the.onClickNext(e);
-                });
-                this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
-                this.clickPrevBtn.disabled = true;
-            },
-            
-            onClickPrev: function (e) {
-                this.clickNextBtn.disabled = false;
-                this.currentImageIndex--;
-                this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
-                if (this.currentImageIndex === 0) {
-                    this.clickPrevBtn.disabled = true;
-                }
-            },
-            
-            onClickNext: function (e) {
-                this.clickPrevBtn.disabled = false;
-                this.currentImageIndex++;
-                this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
-                if (this.currentImageIndex === (this.imagesUrls.length - 1)) {
-                    this.clickNextBtn.disabled = true;
-                }
-            }
-        
-        
-        };
+function Slider() {
+    this.currentImageIndex = 0;
+    this.clickPrevBtn = null;
+    this.clickNextBtn = null;
+    this.imgSlide = null;
+    this.imagesUrls = null;
 
-        return slider;
-    }
+    this.init = function(elId, images) {
+        var the = this;
+        var el = document.querySelector('#' + elId);
+
+        this.clickPrevBtn = el.querySelector('.prevClick');
+        this.clickNextBtn = el.querySelector('.nextClick');
+        this.imgSlide = el.querySelector('.img-slide');
+        this.imagesUrls = images;
+
+
+        // subscribe to events
+        this.clickPrevBtn.addEventListener('click', function (e) {
+            the.onClickPrev(e)
+        });
+        this.clickNextBtn.addEventListener('click', function (e) {
+            the.onClickNext(e);
+        });
+        this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+        this.clickPrevBtn.disabled = true;
+    };
+
+    this.onClickPrev = function (e) {
+        this.clickNextBtn.disabled = false;
+        this.currentImageIndex--;
+        this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+        if (this.currentImageIndex === 0) {
+            this.clickPrevBtn.disabled = true;
+        }
+    };
+
+    this.onClickNext = function (e) {
+        this.clickPrevBtn.disabled = false;
+        this.currentImageIndex++;
+        this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+        if (this.currentImageIndex === (this.imagesUrls.length - 1)) {
+            this.clickNextBtn.disabled = true;
+        }
+    };
+
 };
+
+
+// var sliderFactory = {
+//     getSlider: function () {
+//         var slider = {
+//             currentImageIndex: 0,
+//             clickPrevBtn: null,
+//             clickNextBtn: null,
+//             imgSlide: null,
+//             imagesUrls: null,
+
+//             init: function (elId, images) {
+//                 var the = this;
+//                 var el = document.querySelector('#' + elId);
+
+//                 this.clickPrevBtn = el.querySelector('.prevClick');
+//                 this.clickNextBtn = el.querySelector('.nextClick');
+//                 this.imgSlide = el.querySelector('.img-slide');
+//                 this.imagesUrls = images;
+
+
+//                 // subscribe to events
+//                 this.clickPrevBtn.addEventListener('click', function (e) {
+//                     the.onClickPrev(e)
+//                 });
+//                 this.clickNextBtn.addEventListener('click', function (e) {
+//                     the.onClickNext(e);
+//                 });
+//                 this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+//                 this.clickPrevBtn.disabled = true;
+//             },
+
+//             onClickPrev: function (e) {
+//                 this.clickNextBtn.disabled = false;
+//                 this.currentImageIndex--;
+//                 this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+//                 if (this.currentImageIndex === 0) {
+//                     this.clickPrevBtn.disabled = true;
+//                 }
+//             },
+
+//             onClickNext: function (e) {
+//                 this.clickPrevBtn.disabled = false;
+//                 this.currentImageIndex++;
+//                 this.imgSlide.src = this.imagesUrls[this.currentImageIndex];
+//                 if (this.currentImageIndex === (this.imagesUrls.length - 1)) {
+//                     this.clickNextBtn.disabled = true;
+//                 }
+//             }
+
+
+//         };
+
+//         return slider;
+//     }
+// };
 
 
